d8ad0fcf6e162815dcb38d7ebbe0cfa4796d8dbe
Sherif-lotfy <127263385+Sherif-lotfy@users.noreply.github.com>
2024-02-28 17:02:20 +0200

    Update Q1.cpp
    Signed-off-by: Sherif-lotfy <127263385+Sherif-lotfy@users.noreply.github.com>

--- 1st Dept/ Sheet-1/Q1.cpp
+++ 1st Dept/ Sheet-1/Q1.cpp
@@ -16,57 +16,7 @@ int main() {
 	system("pause");
 	return 0;
 }
-```
-Q1.b
-```
-#include <iostream>
-using namespace std;
-void Test(int a, int b) {
-	if (a < 0) a *= -1;
-	if (b < 0) b *= -1;
-	int c = a, gcf = 1;
-	if (b < a) c = b;
-	for (int i = 1; i <= c; i++) {
-		if (a % i == 0 && b % i == 0) gcf = i;
-	}
-	cout << "integer1 is :" << a << endl << "integer2 is :" << b << endl
-		<< "The Greatest Comman Factor is : " << gcf << endl;
-}
-int main() {
-	int integer1, integer2;
-	cout << "Q1.b\n" << "please enter the first integer\n" << "integer1 is :";
-	cin >> integer1;
-	cout << endl << "please enter the second integer\n" << "integer2 is :";
-	cin >> integer2;
-	cout << endl << endl;
-	Test(integer1, integer2);
-	system("pause");
-	return 0;
-}
-```
-Q1.c
-```
-#include <iostream>
-using namespace std;
-void Test(float a) {
-	int PrimeTest =1;
-	for (int i = a-1; i > 1; i--) {
-		if ((int)a % i == 0) {
-			PrimeTest = i;
-		}
-	}
-	if (PrimeTest != 1) cout << "The Float Number Is Not A Prime Number.\n";
-	else cout << "The Float Number Is A Prime Number.\n";
-}
-int main() {
-	float Number;
-	cout << "Q1.c\n" << "enter The Number You Want To Test\n" << endl << "Num is :";
-	cin >> Number;
-	Test(Number);
-	system("pause");
-	return 0;
-}
-```
+
 Q1.d
 ```
 #include <iostream>
