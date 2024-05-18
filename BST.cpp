c38ab30af3c4bc5a9ca9fab400716e3ed94e477c
Sherif-lotfy <s1h9e7r4if@gmail.com>
2024-05-18 17:55:13 +0300

    update the BST code

--- BST.cpp
+++ BST.cpp
@@ -1,107 +1,190 @@
 #include <iostream>
-#include "list"
-useing namespace std;
+using namespace std;
+
+typedef int elementType;
 
 struct Node
 {
-    int element;
+    elementType element;
     Node *left;
     Node *right;
-    bool isExternal()
+    Node(elementType x = 0, Node *l = NULL, Node *r = NULL)
     {
-        return (right == NULL && left == NULL);
+        element = x;
+        left = l;
+        right = r;
     }
-    list<Node *> children()
+    bool isExternal(Node *p)
     {
-        list<Node *> l;
-        if (left != NULL)
-            l.push_back(left);
-        if (right != NULL)
-            l.push_back(right);
-        return l;
+        if (p->left == NULL && p->right == NULL)
+        {
+            return true;
+        }
+        return false;
     }
 };
 typedef Node *pos;
-class bst
+
+class BST
 {
 public:
     pos root;
-    // int counter;
-    bst() { root = NULL; }
-    bst(int e, pos l, pos r)
+    BST()
     {
-        root->element = e;
-        root->left = l;
-        root->right = r;
+        root = NULL;
     }
-    void insert(pos &p, int element)
-    {
-        if (p->isExternal())
-        {
-            p = New Node(e, NULL, NULL)
-        }
-        else
-        {
-            if (e < p->element)
-            {
-                inset(p->left, e);
-            }
-            else
-            {
-                inset(p->right, e);
-            }
-        }
-    }
-    bool find(pos p, int e)
+    void insert(pos &p, elementType e)
     {
         if (p == NULL)
         {
-            return false;
+            p = new Node(e, NULL, NULL);
         }
         else
         {
-            if (e == p->element)
+            if (p->element > e)
             {
-                return true;
+                insert(p->left, e);
             }
-            else if (e < p->element)
+            else if (p->element < e)
             {
-                return find(p->left, e);
+                insert(p->right, e);
+            }
+        }
+        // if (p->element == e)
+        // {
+        //     return;
+        // }
+        // else if (p->element > e)
+        // {
+        //     if (p->left == NULL)
+        //     {
+        //         Node *newnode = new Node;
+        //         p->left = newnode;
+        //         newnode->element = e;
+        //     }
+        //     else
+        //     {
+        //         insert(p->left, e);
+        //     }
+        // }
+        // else if (p->element < e)
+        // {
+        //     if (p->right == NULL)
+        //     {
+        //         Node *newnode = new Node;
+        //         p->right = newnode;
+        //         newnode->element = e;
+        //     }
+        //     else
+        //     {
+        //         insert(p->right, e);
+        //     }
+        // }
+    }
+    pos findMin(pos p)
+    {
+        if (p == NULL)
+        {
+            return NULL;
+        }
+        else
+        {
+            pos current = p;
+            while (current->left != NULL)
+            {
+                current = current->left;
+            }
+            return current;
+        }
+    }
+    void remove(pos &ptr, elementType elem)
+    {
+        if(ptr == NULL) return;
+        if (ptr->element == elem){
+            if (ptr->left != NULL && ptr->right != NULL)
+            {
+                pos p =  findMin(ptr->right);
+                ptr->element = p->element;
+                remove(ptr->right, p->element);
             }
             else
             {
-                return find(p->right, e)
+                if (ptr->isExternal(ptr))
+                {
+                    ptr = NULL;
+                }
+                else if (ptr->left != NULL)
+                {
+                    pos discard = ptr;
+                    ptr = ptr->left;
+                    delete discard;
+                }
+                else
+                {
+                    pos discard = ptr;
+                    ptr = ptr->right;
+                    delete discard;
+                }
             }
         }
+        else if (ptr->element > elem)
+            remove(ptr->left, elem);
+        else
+            remove(ptr->right, elem);
     }
-   Node minNode(pos p){
-    pos current = p;
-    while(current->left !=NULL){
-        current = current->left;
+    void inorder(pos p)
+    {
+        if (p != NULL)
+        {
+            inorder(p->left);
+            cout << " " << p->element << " ";
+            inorder(p->right);
+        }
     }
-    return current;
-   }
-    void Delete(pos p){
-        if(p->left == NULL && p->right == NULL){
-            p = NULL;
+    void preorder(pos p)
+    {
+        if (p != NULL)
+        {
+            cout << " " << p->element << " ";
+            preorder(p->left);
+            preorder(p->right);
         }
-        else if((p->left == NULL && p->right != NULL) || (p->left != NULL && p->right == NULL)){
-            if(p->right == NULL){
-
-                //i have no idea...
-                
-            }
-        }
-        else if(p->left == NULL && p->right == NULL){
-            pos minright = minNode(p->right);
-            p->element = minright->element;
-            minright = NULL;
+    }
+    void postorder(pos p)
+    {
+        if (p != NULL)
+        {
+            postorder(p->left);
+            postorder(p->right);
+            cout << " " << p->element << " ";
         }
     }
 };
 
 int main()
 {
-
+    BST t;
+    t.insert(t.root, 4);
+    t.insert(t.root, 2);
+    t.insert(t.root, 1);
+    t.insert(t.root, 3);
+    t.insert(t.root, 6);
+    t.insert(t.root, 5);
+    t.insert(t.root, 7);
+    cout << "\n minimum value in BST is: " << t.findMin(t.root)->element;
+    cout<<endl;
+    t.inorder(t.root);
+    cout<<endl;
+    t.postorder(t.root);
+    cout<<endl;
+    t.preorder(t.root);
+    t.remove(t.root, 4);
+    cout<<endl;
+    t.inorder(t.root);
+    cout<<endl;
+    t.postorder(t.root);
+    cout<<endl;
+    t.preorder(t.root);
+    system("pause");
     return 0;
 }
